eab76d2515898f9f3b7a6cedebf1f19fd35c6600
rameshpaswan44 <raj168ramesh@gmail.com>
2026-08-12 12:39:01 +0545

    api modified

--- Buddha-Hr-frontend/buddha-hr-frontend/src/services/api.js
+++ Buddha-Hr-frontend/buddha-hr-frontend/src/services/api.js
@@ -1,28 +1,11 @@
-// import axios from "axios";
+
+import axios from "axios";
 
 // const api = axios.create({
 //     baseURL: "http://localhost:8080/api",
 // });
-
-// api.interceptors.request.use((config) => {
-
-//     const token = localStorage.getItem("token");
-
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-
-//     return config;
-
-// });
-
-// export default api;
-
-
-import axios from "axios";
-
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 // Attach JWT to every request
